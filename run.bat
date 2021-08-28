bd94e69543d6cfaf666ece4965dcfeb52b3a545c
qinhuayi <qinhuayi@qq.com>
2021-08-28 18:48:47 +0800

    Added server.js and 2 shell command files for test de.js by running project directory as a static website under node.js environment.

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+:: Run project directory as a static website for test de.js
+:: 运行项目目录作为一个静态网站以便测试de.js
+node server.js
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run project directory as a static website for test de.js 
+# 运行项目目录作为一个静态网站以便测试de.js
+node server.js
