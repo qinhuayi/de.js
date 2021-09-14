d2e0cc9bd7f502b7e6f2b17882571ec97cfb52a5
qinhuayi <qinhuayi@qq.com>
2021-09-14 17:39:04 +0800

    Added 2 shell script files for minify de.js

---
+++ min.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo compress es5/de.js to es5/de.min.js
+uglifyjs es5/de.js -m -c -o "es5/de.min.js"
+echo compress es6/de.js to es6/de.min.js
+uglifyjs es6/de.js -m -c -o "es6/de.min.js"
